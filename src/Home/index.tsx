38e62af18be97e40e3088e62a22a20a67ab2ee93
Deuce Black <deuce2black@gmail.com>
2023-08-10 17:17:33 -0500

    added basic routing as per #3

---
+++ src/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='text-2xl text-blue-700'>
+            hello world
+        </div>
+    );
+};
+
+export default Home;
